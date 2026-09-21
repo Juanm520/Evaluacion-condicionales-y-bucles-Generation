dfabb599b18e6d8929e42727b4ff1ec897ae6633
Juan M. Rodriguez Lanza <juanma_520@hotmail.com>
2026-09-21 14:08:16 -0500

    Terminada la task 5

--- task5.js
+++ task5.js
@@ -3,35 +3,72 @@ const prompt = require('prompt-sync')();
 
 //Es especifica o lineas
 
-let esEspecificaPrompt = prompt('Desea ingresar numero de lineas o una linea especifica (l ó e): ')
+let esEspecificaPrompt = prompt('Desea ingresar numero de lineas o una linea especifica (L/E): ')
 
 while (esEspecificaPrompt.toLowerCase() !== 'l' && esEspecificaPrompt.toLowerCase() !== 'e') {
-  esEspecificaPrompt = prompt('Desea ingresar numero de lineas o una linea especifica (l ó e): ')
-  console.log('Opción inválida. Por favor, ingrese "l" para lineas o "e" para especifica.')
+  console.log('Opción inválida. Por favor, ingrese "L" para lineas o "E" para especifica.')
+  esEspecificaPrompt = prompt('Desea ingresar numero de lineas o una linea especifica (L/E): ')
 }
 
-const esEspecifica = esEspecificaPrompt.toLowerCase() === 'l' : 
+if (esEspecificaPrompt.toLowerCase() === 'l') {
 
+  let numLineas = parseInt(prompt('Ingrese el número de lineas: '))
 
-for (let i = 0; i <= 105; i++) {
-
- let salida = ''
-
- if (i % 3 === 0) {
-    salida = salida + 'Fizz'
+  while (isNaN(numLineas) || numLineas <= 0) {
+    console.log('Opción inválida. Por favor, ingrese un número válido.')
+    numLineas = parseInt(prompt('Ingrese el número de lineas: '))
   }
 
-  if (i % 5 === 0) {
-    salida = salida +  'Buzz'
-  } 
-  
-  if(i % 7 === 0) {
-    salida = salida + 'Woof'
+  for (let i = 0; i <= numLineas; i++) {
+
+    let salida = ''
+
+    if (i % 3 === 0) {
+      salida = salida + 'Fizz'
+    }
+
+    if (i % 5 === 0) {
+      salida = salida +  'Buzz'
+    } 
+    
+    if(i % 7 === 0) {
+      salida = salida + 'Woof'
+    }
+
+    if (salida === '') {
+      console.log(i)
+    } else {
+      console.log(salida)
+    } 
+  }
+} else {
+
+  let lineaEspecifica = parseInt(prompt('Ingrese la línea específica: '))
+
+  while (isNaN(lineaEspecifica) || lineaEspecifica < 0) {
+    console.log('Opción inválida. Por favor, ingrese un número válido.')
+    lineaEspecifica = parseInt(prompt('Ingrese la línea específica: '))
   }
 
-  if (salida === '') {
-    console.log(i)
-  } else {
-    console.log(salida)
-  } 
-};
+  let salida = ''
+
+    if (lineaEspecifica % 3 === 0) {
+      salida = salida + 'Fizz'
+    }
+
+    if (lineaEspecifica % 5 === 0) {
+      salida = salida +  'Buzz'
+    } 
+    
+    if(lineaEspecifica % 7 === 0) {
+      salida = salida + 'Woof'
+    }
+
+    if (salida === '') {
+      console.log(lineaEspecifica)
+    } else {
+      console.log(salida)
+    }
+}
+
+   
